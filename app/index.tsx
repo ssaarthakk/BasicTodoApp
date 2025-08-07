8bcfa7eab8afaf3435ba791ea83218060eebf390
Sarthak Kumar <realsarthakkumar@gmail.com>
2025-08-07 21:47:09 +0530

    feat: reset project structure and set up Firebase configuration with environment variables.

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <View className='flex-1 items-center justify-center bg-gray-600'>
+        <Text className='text-white'>Hello World!</Text>
+      </View>
+    </>
+  );
+}
